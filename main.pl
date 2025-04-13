c107c7bab48275a883070ee721fe1c0264ee3d59
ietzel <67884277+ietzel@users.noreply.github.com>
2025-04-12 21:26:25 -0400

    Update and rename main.scala to main.pl

---
+++ main.pl
@@ -0,0 +1,13 @@
+use strict;
+use warnings;
+
+//alignment, enneagram, feng shui, gardner's intelligences, industries worldwide, etc.
+print("LN, 1, work, spatial, Financial Intermediation\n");
+print("LG, 2, relationsips, interpersonal, Telecommunications\n");
+print("LE, 3, wealth, bodily-kinesthetic, Mining\n");
+print("CG, 4, youthfulness, intrapersonal, Community/Social/Personal Service Activities\n");
+print("NE, 5, knowledge, logical-mathematical, Business Activities\n");
+print("N, 6, health, existential, Manufacturing\n");
+print("CN, 7, travel, musical, Transport/Post/Storage\n");
+print("CE, 8, fame, naturalistic, Real Estate + Renting + Hotels + Restaurants\n");
+print("NG, 9, family, linguistic, Wholesale/Retail Trade");
